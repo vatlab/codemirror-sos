911d016ae47da2451e8b9b6c1972c9b69418d998
Bo Peng <ben.bob@gmail.com>
2023-09-29 23:15:42 -0500

    add python syntax

--- src/sos.ts
+++ src/sos.ts
@@ -1,4 +1,14 @@
-import { parser, configureNesting } from "@lezer/html"
+
+import {parser} from "@lezer/python"
+import {SyntaxNode} from "@lezer/common"
+import {delimitedIndent, indentNodeProp, TreeIndentContext,
+        foldNodeProp, foldInside, LRLanguage, LanguageSupport} from "@codemirror/language"
+import {globalCompletion, localCompletionSource} from "./complete"
+export {globalCompletion, localCompletionSource}
+
+
+
+import { parser, configureNesting } from "@lezer/python"
 import { Parser } from "@lezer/common"
 
 import { pythonLanguage  } from "@codemirror/lang-python"
@@ -6,7 +16,7 @@ import { rLanguage } from "codemirror-lang-r"
 import { markdownLanguage } from "@codemirror/lang-markdown"
 import { javascriptLanguage} from "@codemirror/lang-javascript"
 
-import {StreamLanguage} from "@codemirror/language"
+import { StreamLanguage } from "@codemirror/language"
 import { ruby } from "@codemirror/legacy-modes/mode/ruby"
 import { sas } from "@codemirror/legacy-modes/mode/sas"
 import { octave } from "@codemirror/legacy-modes/mode/octave"
@@ -82,6 +92,80 @@ const defaulAction: ActionLang[] = [
   }
 ]
 
+function indentBody(context: TreeIndentContext, node: SyntaxNode) {
+  let base = context.baseIndentFor(node)
+  let line = context.lineAt(context.pos, -1), to = line.from + line.text.length
+  // Don't consider blank, deindented lines at the end of the
+  // block part of the block
+  if (/^\s*($|#)/.test(line.text) &&
+      context.node.to < to + 100 &&
+      !/\S/.test(context.state.sliceDoc(to, context.node.to)) &&
+      context.lineIndent(context.pos, -1) <= base)
+    return null
+  // A normally deindenting keyword that appears at a higher
+  // indentation than the block should probably be handled by the next
+  // level
+  if (/^\s*(else:|elif |except |finally:)/.test(context.textAfter) && context.lineIndent(context.pos, -1) > base)
+    return null
+  return base + context.unit
+}
+
+/// A language provider based on the [Lezer Python
+/// parser](https://github.com/lezer-parser/python), extended with
+/// highlighting and indentation information.
+export const pythonLanguage = LRLanguage.define({
+  name: "python",
+  parser: parser.configure({
+    props: [
+      indentNodeProp.add({
+        Body: context => indentBody(context, context.node) ?? context.continue(),
+        IfStatement: cx => /^\s*(else:|elif )/.test(cx.textAfter) ? cx.baseIndent : cx.continue(),
+        TryStatement: cx => /^\s*(except |finally:|else:)/.test(cx.textAfter) ? cx.baseIndent : cx.continue(),
+        "TupleExpression ComprehensionExpression ParamList ArgList ParenthesizedExpression": delimitedIndent({closing: ")"}),
+        "DictionaryExpression DictionaryComprehensionExpression SetExpression SetComprehensionExpression": delimitedIndent({closing: "}"}),
+        "ArrayExpression ArrayComprehensionExpression": delimitedIndent({closing: "]"}),
+        "String FormatString": () => null,
+        Script: context => {
+          if (context.pos + /\s*/.exec(context.textAfter)![0].length >= context.node.to) {
+            let endBody = null
+            for (let cur: SyntaxNode | null = context.node, to = cur.to;;) {
+              cur = cur.lastChild
+              if (!cur || cur.to != to) break
+              if (cur.type.name == "Body") endBody = cur
+            }
+            if (endBody) {
+              let bodyIndent = indentBody(context, endBody)
+              if (bodyIndent != null) return bodyIndent
+            }
+          }
+          return context.continue()
+        }
+      }),
+      foldNodeProp.add({
+        "ArrayExpression DictionaryExpression SetExpression TupleExpression": foldInside,
+        Body: (node, state) => ({from: node.from + 1, to: node.to - (node.to == state.doc.length ? 0 : 1)})
+      })
+    ],
+  }),
+  languageData: {
+    closeBrackets: {
+      brackets: ["(", "[", "{", "'", '"', "'''", '"""'],
+      stringPrefixes: ["f", "fr", "rf", "r", "u", "b", "br", "rb",
+                       "F", "FR", "RF", "R", "U", "B", "BR", "RB"]
+    },
+    commentTokens: {line: "#"},
+    indentOnInput: /^\s*([\}\]\)]|else:|elif |except |finally:)$/
+  }
+})
+
+/// Python language support.
+export function python() {
+  return new LanguageSupport(pythonLanguage, [
+    pythonLanguage.data.of({autocomplete: localCompletionSource}),
+    pythonLanguage.data.of({autocomplete: globalCompletion}),
+  ])
+}
+
 export const SoS = LRLanguage.define({
   name: "SoS",
   parser: parser.configure({
@@ -122,9 +206,6 @@ export const SoS = LRLanguage.define({
     ]
   }),
   languageData: {
-    commentTokens: { block: { open: "<!--", close: "-->" } },
-    indentOnInput: /^\s*<\/\w+\W$/,
-    wordChars: "-._"
   }
 })
 
